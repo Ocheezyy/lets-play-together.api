e554ee1ff7c9f4ad05050aa2dcafb5d9e0030cc3
ocheezyyy <seanodonnellse@gmail.com>
2025-04-30 11:04:56 -0400

    Updated steam routes

--- routers/steam.py
+++ routers/steam.py
@@ -11,14 +11,8 @@ from globals import STEAM_API_KEY
 steam_router = APIRouter()
 
 
-@steam_router.get("/steam/friends")
-async def get_steam_friends(
-    session: AsyncSession = Depends(get_session),
-    current_user: User = Depends(get_current_user),
-):
-    if not current_user.steam_id:
-        raise HTTPException(400, "Steam account not linked.")
-
+@steam_router.get("/friends")
+async def get_steam_friends(current_user: User = Depends(get_current_user)):
     steam_id = current_user.steam_id
     url = "https://api.steampowered.com/ISteamUser/GetFriendList/v1/"
     params = {"key": STEAM_API_KEY, "steamid": steam_id, "relationship": "friend"}
@@ -33,10 +27,10 @@ async def get_steam_friends(
     return data["friendslist"]["friends"]
 
 
-@steam_router.get("/steam_games/{steam_id}")
-async def route_get_steam_games(steam_id: int):
+@steam_router.get("/games")
+async def route_get_steam_games(current_user: User = Depends(get_current_user),):
     async with AsyncClient() as client:
         response = await client.get(
-            f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={STEAM_API_KEY}&steamid={steam_id}&include_appinfo=true&include_played_free_games=true"
+            f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={STEAM_API_KEY}&steamid={current_user.steam_id}&include_appinfo=true&include_played_free_games=true"
         )
         return response.json()
